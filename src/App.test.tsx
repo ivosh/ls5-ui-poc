a2c552be4333ae1cb8aa1f4bd6bddfe4de4bfe34
Ivo Raisr <ivosh@ivosh.net>
2018-12-30 16:54:21 +0100

    Use react-test-renderer for testing with Jest.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import TestRenderer from 'react-test-renderer';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const component = TestRenderer.create(<App />);
+  expect(component.toJSON()).toMatchSnapshot();
 });
